6caf1659b35c6f58ed6ed2695365b75054f1f845
Danyon <Danyongreymoon@gmail.com>
2016-10-23 17:46:52 -0600

    Fixing the base project.

---
+++ src/StateMachines/State.java
@@ -0,0 +1,25 @@
+package StateMachines;
+
+/**
+ * Created by Greymoon on 10/23/2016.
+ */
+public abstract class State {
+    private boolean active;
+    protected String tag = "UN-NAMED";
+    public abstract void OnStateEnter();
+    public abstract void OnStateUpdate();
+    public abstract void OnStateExit();
+
+    public boolean isActive(){
+        return active;
+    }
+    public void activate(){
+        active = true;
+    }
+    public void deactivate(){
+        active = false;
+    }
+    public String getTag(){
+        return tag;
+    }
+}
