e01e4ef55dfc9f4d349ca4acccc1ea85db85ceb8
MShultz <mshultz@student.neumont.edu>
2016-11-09 14:15:35 -0700

    Addid MartianSystem

---
+++ LittleChewChewToSpace/src/MartianSystem.java
@@ -0,0 +1,17 @@
+import lejos.nxt.Sound;
+
+public class MartianSystem{
+	private final static int FORWARD_SOUND_CODE = 3;
+	private final static int BACKWARD_SOUND_CODE = 2;
+	private final static int TOUCHING_SOUND_CODE = 4;
+	
+	public void playForwardSound(){
+		Sound.systemSound(false, FORWARD_SOUND_CODE);
+	}
+	public void playBackwardSound(){
+		Sound.systemSound(false, BACKWARD_SOUND_CODE);
+	}
+	public void playTouchingSound(){
+		Sound.systemSound(false, TOUCHING_SOUND_CODE);
+	}
+}
