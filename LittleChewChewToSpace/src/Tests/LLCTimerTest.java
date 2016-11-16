81941d542de7ee7f0b4e219d9b3ad7d1bb9e19f9
Alexander <alhuertas@student.neumont.edu>
2016-11-16 11:54:42 -0700

    Hopefully flushed out the testcases

--- LittleChewChewToSpace/src/Tests/LLCTimerTest.java
+++ LittleChewChewToSpace/src/Tests/LLCTimerTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class LLCTimerTest implements Runnable {
     
-	private long currentTimeInMilliseconds;
+	private long currentTimeInMilliseconds = 0;
 	private boolean running = true;
 	private Thread timerThread;
 	
@@ -15,15 +15,18 @@ public class LLCTimerTest implements Runnable {
 	//Assert ALL boundary coverage
 	@Test
 	public void testRun() {
-		Assert.assertNull(currentTimeInMilliseconds);
-		currentTimeInMilliseconds = 0;
+		Assert.assertNotEquals(100,currentTimeInMilliseconds);
+		
 		
 		Assert.assertTrue(running);
+		timerThread = new Thread();
 		while(running) {
 			
 			trySleep(1);
+			
+			timerThread.start();
 			Assert.assertFalse(timerThread.isInterrupted());
-			Thread.interrupted();
+			timerThread.interrupted();
 			Assert.assertTrue(timerThread.isInterrupted());
 			
 			Assert.assertEquals(0, currentTimeInMilliseconds);
@@ -39,24 +42,14 @@ public class LLCTimerTest implements Runnable {
 	
 	@Test 
 	public void testSleep() {
-		try {
-			
-			Thread t = new Thread() {
-				public void run() {
-					trySleep(1000);
-				}
-			};
-			
-			t.start();
-			Thread.sleep(1000);
-			Assert.assertFalse(t.isInterrupted());
-			t.interrupt();
-			Assert.assertTrue(t.isInterrupted());
-			
-			
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		Thread t = new Thread() {
+			public void run() {
+				trySleep(1000);
+			}
+		};
+		Assert.assertFalse(t.isInterrupted());
+		t.interrupt();
+		Assert.assertFalse(t.isInterrupted());
 		
 	}
 	private void trySleep(long ms){
@@ -64,15 +57,14 @@ public class LLCTimerTest implements Runnable {
 			Thread.sleep(ms);
 		} catch (InterruptedException e) {
 			System.out.println("Thread exception: " + e);
-			e.printStackTrace();
 		}
 	}
 
 	@Test
 	public void testStartTimer() {
-		  Assert.assertNull(currentTimeInMilliseconds);
+		  Assert.assertEquals(0 ,currentTimeInMilliseconds);
 		  currentTimeInMilliseconds = 100;
-		  Assert.assertEquals(0, currentTimeInMilliseconds);
+		  Assert.assertEquals(100, currentTimeInMilliseconds);
 		  
 		  Assert.assertNull(timerThread);
 		  timerThread = new Thread(this,"Timer");
@@ -87,7 +79,7 @@ public class LLCTimerTest implements Runnable {
 			
 			e.printStackTrace();
 		}
-		  Assert.assertEquals(100, currentTimeInMilliseconds);
+		  Assert.assertNotEquals(100, currentTimeInMilliseconds);
 		  Assert.assertNotEquals(1500, currentTimeInMilliseconds);
 		  
 		  Assert.assertEquals(0, currentTimeInMilliseconds);
